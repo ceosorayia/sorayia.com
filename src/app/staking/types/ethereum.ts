8ead0211c6727306dcc51f6e10d83045753ef0f1
sorayia.com <161327848+ceosorayia@users.noreply.github.com>
2024-11-26 17:46:38 +0100

    Update ethereum.ts

--- src/app/staking/types/ethereum.ts
+++ src/app/staking/types/ethereum.ts
@@ -1,21 +1,5 @@
 export interface Web3Error {
-  code: number;
-  message: string;
-}
-
-export interface TokenConfig {
-  address: string;
-  abi: string[];
-  priceInBNB: number;
-}
-
-export interface TokenPrice {
-  amount: string;
-  bnbEquivalent: string;
-}
-
-declare global {
-  interface Window {
-    ethereum?: any;
-  }
+  code: number | string;
+  message?: string;
+  data?: unknown;
 }
