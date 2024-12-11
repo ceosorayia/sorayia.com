234173b99f75c7801778c79c00d35bf953dd6d01
sorayia.com <161327848+ceosorayia@users.noreply.github.com>
2024-12-11 23:38:25 +0100

    Update config.ts

--- src/app/staking/contracts/config.ts
+++ src/app/staking/contracts/config.ts
@@ -1,4 +1,4 @@
 export const TOKEN_ADDRESS: string = '0x424516fBe635c5642723AB6c1b413fF61B177dd6';
-export const STAKING_ADDRESS: string = '0x5fd4b018cd0396711774dd5edb0c6d2e49855e5f';
+export const STAKING_ADDRESS: string = '0x037c24C4A032f2f274b87f7426c90f808A061603';
 
 export const TOKEN_DECIMALS: number = 18; // Standard ERC20 decimals
