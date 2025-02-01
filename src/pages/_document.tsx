c09df4c3046d9a6c035d432a14ec833aa20b1977
sorayia.com <161327848+ceosorayia@users.noreply.github.com>
2025-02-01 14:42:31 +0100

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,19 +4,17 @@ export default function Document() {
   return (
     <Html lang="fr">
       <Head>
-        {/* Google Analytics */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-DZFNTSB0LF"
-        />
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-DZFNTSB0LF"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-DZFNTSB0LF');
-            `,
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-DZFNTSB0LF');
+`,
           }}
         />
       </Head>
